3bc4f816fbd6681040de37c88213284a880b8c0f
Winnypohh <maik523@hotmail.com>
2022-12-17 11:23:38 +0000

    changed nav bar

--- myblog/admin.py
+++ myblog/admin.py
@@ -1,11 +1,12 @@
+'''  Post and commments and admin approvements '''
 from django.contrib import admin
-from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
+from .models import Post, Comment
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-
+    ''' postadmin comment approval requirements and summernote '''
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
     list_filter = ('status', 'created_on')
@@ -15,10 +16,12 @@ class PostAdmin(SummernoteModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
+    ''' admin requirements for comment approval  '''
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'body')
     actions = ['approve_comments']
 
-    def approve_comments(self, request, queryset):
+    def approve_comments(self, request, queryset):  # unused:request used later
+        ''' Request to approve comment '''
         queryset.update(approved=True)
